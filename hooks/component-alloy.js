3c3a378825f86f191eec6dbedf3134a2c3eb4022
btknorr <btknorr@gmail.com>
2013-08-13 14:11:44 -0500

    titanium plugin to use component build in alloy

---
+++ hooks/component-alloy.js
@@ -0,0 +1,37 @@
+exports.cliVersion = '>=3.X';
+
+exports.init = function (logger, config, cli, appc) {
+
+    var exec = require('child_process').exec;
+
+    cli.addHook('build.pre.compile', function (build, finished) {
+        var appDir = build.projectDir + '/app';
+        logger.info("Running component-alloy plugin...");
+
+        exec('cd "' + appDir + '" && ulimit -n 10000 && component build -v -c -o lib -n component', function (error, stdout, stderr) {
+            logger.info(stdout);
+            if (stderr.length) {
+                logger.error(stderr);
+                return process.exit(1);
+            }
+            if (error != null) {
+                logger.error(error);
+                return process.exit(1);
+            }
+            var fs = require('fs');
+            return fs.appendFile(appDir+'/lib/component.js', '\nmodule.exports = require;', function(err) {
+                if (err) {
+                    logger.error(error);
+	                return process.exit(1);
+                }
+                var component = require(appDir+'/lib/component');
+                var code = "";
+                for (var key in component.aliases) {
+                    var path = component.aliases[key];
+                    code += '\nrequire.alias("'+path+'","'+(key.split('deps/')[1])+'");';
+                }
+                return fs.appendFile(appDir+'/lib/component.js', code, finished);
+            });
+        });
+    });
+};
